a625912a3854604d55d88022d4dd9c1eda6363fc
Zalarast <shubin.bodya@gmail.com>
2022-07-20 16:23:53 +0300

    Удаление cookie при выходе из аккаунта

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,11 +1,19 @@
 import { NavMenu } from "../../components";
 import { LoginProps } from "../../types";
+import { removeCoockie } from "../../utils";
 export default function Main({ auth }: LoginProps) {
   return (
     <div>
       <NavMenu />
       1231231
-      <button onClick={auth}>CLOSE</button>
+      <button
+        onClick={() => {
+          auth();
+          removeCoockie("token");
+        }}
+      >
+        CLOSE
+      </button>
     </div>
   );
 }
