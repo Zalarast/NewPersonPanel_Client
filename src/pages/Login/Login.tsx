fe581e70cc955fe9a71c709f5c5592983890834a
Zalarast <shubin.bodya@gmail.com>
2022-07-20 16:25:32 +0300

    Авторизация при нажатии на Enter, автоматическая авторизация при наличии cookie

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import "../../CSS/Login.css";
 import { AuthData, LoginProps } from "../../types";
+import { getCookie } from "../../utils";
 import { authorization } from "./functions";
 
 export default function Login({ auth }: LoginProps) {
+  React.useEffect(() => {
+    authorization(getCookie("token"))
+      .then((res) => res && auth())
+      .catch((err) => console.error("При авторизации произошла ошибка:", err));
+  });
   const [authData, setAuthData] = React.useState<AuthData>({
     login: "",
     password: "",
@@ -17,15 +23,15 @@ export default function Login({ auth }: LoginProps) {
 
   const loginIn = async () => {
     if (authData.login && authData.password) {
-      const result = await authorization(authData);
+      const result = await authorization(null, authData);
       if (result) auth();
       else alert("Неизвестный пользователь");
     } else alert("Ошибка, введены не все данные");
   };
 
   return (
-    <div className="Login">
-      <h1>Авторизация</h1>
+    <div className="Login" onKeyDown={(e) => e.key === "Enter" && loginIn()}>
+      <h1 className="loginTitle">Авторизация</h1>
       <div>
         <h2>Логин</h2>
         <input name="login" onChange={handleChange} />
