2bb8aa7816d890288dfc9795706e5ce10eb74f91
Zalarast <shubin.bodya@gmail.com>
2022-07-22 17:58:34 +0300

    Исправил тег a на Link для нормальной переадресации по страницам

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -1,13 +1,14 @@
 import "../CSS/NavMenu.css";
 import { NavMenuProps } from "../types";
+import { Link } from "react-router-dom";
 
 export default function NavMenu({ stateOpen, open }: NavMenuProps) {
   return (
     <div className={`NavMenu ${stateOpen ? "open" : "close"}`}>
       <h1>Панель Управления</h1>
       <div className="navList">
-        <a href="#">Добавление в Active Directory</a>
-        <a href="#">Добавление в Bitrix24</a>
+        <Link to="/ActiveDirectory" onClick={open}>Добавление в Active Directory</Link>
+        <Link to="/Bitrix24" onClick={open}>Добавление в Bitrix24</Link>
       </div>
       <button onClick={open}>Закрыть панель</button>
     </div>
