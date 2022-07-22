9d4a632164ac05a166659d12467e2cc51b850638
Zalarast <shubin.bodya@gmail.com>
2022-07-22 18:05:22 +0300

    Очищение пути при выходе из учётки

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -1,11 +1,12 @@
 import { HeadProps } from "../types";
 import "../CSS/Head.css";
+import { Link } from "react-router-dom";
 
 export default function Head({ openMenu, reAuth }: HeadProps) {
   return (
     <div className="Head">
       <button onClick={openMenu}>Открыть панель</button>
-      <button onClick={reAuth}>Выйти из учётки</button>
+      <Link to="/"><button onClick={reAuth}>Выйти из учётки</button></Link>
     </div>
   );
 }
