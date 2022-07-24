ea559f16f33aca8bf54ac477d4b72ed93b5d4aff
Zalarast <shubin.bodya@gmail.com>
2022-07-24 21:23:49 +0300

    Форматировани

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -7,8 +7,12 @@ export default function NavMenu({ stateOpen, open }: NavMenuProps) {
     <div className={`NavMenu ${stateOpen ? "open" : "close"}`}>
       <h1>Панель Управления</h1>
       <div className="navList">
-        <Link to="/ActiveDirectory" onClick={open}>Добавление в Active Directory</Link>
-        <Link to="/Bitrix24" onClick={open}>Добавление в Bitrix24</Link>
+        <Link to="/ActiveDirectory" onClick={open}>
+          Добавление в Active Directory
+        </Link>
+        <Link to="/Bitrix24" onClick={open}>
+          Добавление в Bitrix24
+        </Link>
       </div>
       <button onClick={open}>Закрыть панель</button>
     </div>
