2b393710ef54dfbb18c63536fd499ab54e474af1
Zalarast <shubin.bodya@gmail.com>
2022-07-25 18:03:58 +0300

    Передача функции для изменения State с данными об пользователе

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -3,7 +3,7 @@ import "../../CSS/Login.css";
 import { AuthData, LoginProps } from "../../types";
 import { authorization } from "./functions";
 
-export default function Login({ auth, login, children }: LoginProps) {
+export default function Login({ auth, login, setUserInfo, children }: LoginProps) {
   const [authData, setAuthData] = React.useState<AuthData>({
     login: "",
     password: "",
@@ -15,7 +15,7 @@ export default function Login({ auth, login, children }: LoginProps) {
 
   const loginIn = async () => {
     if (authData.login && authData.password) {
-      const result = await authorization(authData);
+      const result = await authorization(authData, setUserInfo);
       if (result) auth();
       else alert("Неизвестный пользователь");
     } else alert("Ошибка, введены не все данные");
