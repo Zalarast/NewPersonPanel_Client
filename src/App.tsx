ea747479542675d77b8d10a6255e8f559442f3fc
Zalarast <shubin.bodya@gmail.com>
2022-07-25 18:01:31 +0300

    State для хранения об информации о пользоватле

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,24 @@
 import React from "react";
 import { Login, Main } from "./pages";
 import { autoAuthorization } from "./pages/Login/functions";
-import { getCookie } from "./utils";
+import { userInfo } from "./types"
 
 function App() {
   const [login, setLogin] = React.useState(false);
   const setAuth = () => setLogin(!login);
+  const [userInfo, setUserInfo] = React.useState<userInfo>({});
 
   // eslint-disable-next-line
   React.useEffect(() => {
-    const token = getCookie("token");
-    if (token) {
-      autoAuthorization(token)
-        .then((res) => res && setAuth())
-        .catch((err) =>
-          console.error("При авторизации произошла ошибка:", err)
-        );
-    }
+    autoAuthorization()
+      .then((res) => res && setAuth())
+      .catch((err) => console.error("При авторизации произошла ошибка:", err));
   }, []);
 
   return (
     <div className="App">
-      <Login auth={setAuth} login={login}>
-        <Main auth={setAuth} />
+      <Login auth={setAuth} login={login} setUserInfo={setUserInfo}>
+        <Main auth={setAuth} userInfo={userInfo} />
       </Login>
     </div>
   );
