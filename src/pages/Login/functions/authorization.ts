1a5defaeace89cb15acdad641136bdf8890e2232
Zalarast <shubin.bodya@gmail.com>
2022-07-25 18:02:18 +0300

    Изменил куки token на UID и доавил PID для логина

--- src/pages/Login/functions/authorization.ts
+++ src/pages/Login/functions/authorization.ts
@@ -1,25 +1,33 @@
 import axios from "axios";
-import { setCookie, serverUrl } from "../../../utils";
+import { userInfo } from "../../../types";
+import { setCookie, serverUrl, getCookie } from "../../../utils";
 
-export async function authorization(authData: {
-  login: string;
-  password: string;
-}): Promise<boolean> {
+export async function authorization(
+  authData: {
+    login: string;
+    password: string;
+  },
+  setUserInfo: React.Dispatch<React.SetStateAction<userInfo>>
+): Promise<boolean> {
   const res = await axios({
     url: serverUrl() + "/auth",
     data: { ...authData, reauth: true },
     method: "POST",
   });
-  if (res.data.token) setCookie("token", res.data.token);
+  res.data.token && setCookie("UID", res.data.token);
+  res.data.userInfo.login && setCookie("PID", res.data.userInfo.login);
+  setUserInfo({ ...res.data.userInfo });
   return res.data.result;
 }
 
-export async function autoAuthorization(token: string) {
-  const res = await axios({
-    url: serverUrl() + "/auth",
-    data: { token: token, reauth: false },
-    method: "POST",
-  });
-  if (res.data.token) setCookie("token", res.data.token);
-  return res.data.result;
+export async function autoAuthorization() {
+  if (getCookie("UID")) {
+    const res = await axios({
+      url: serverUrl() + "/auth",
+      data: { token: getCookie("UID"), reauth: false },
+      method: "POST",
+    });
+    if (res.data.token) setCookie("UID", res.data.token);
+    return res.data.result;
+  } else return false;
 }
