c332de21edf282cc8f0a5d1fe2d60e116995dec5
Zalarast <shubin.bodya@gmail.com>
2022-07-25 18:02:39 +0300

    Вариативность удаления сразу нескольких токенов

--- src/utils/cookie.ts
+++ src/utils/cookie.ts
@@ -10,6 +10,7 @@ export function setCookie(coockieKey: string, token: string) {
   cookies.set(coockieKey, token);
 }
 
-export function removeCoockie(coockieKey: string) {
-  cookies.remove(coockieKey);
+export function removeCoockie(coockieKey: string[] | string) {
+  if (typeof coockieKey === "string") cookies.remove(coockieKey);
+  else for (const key of coockieKey) cookies.remove(key);
 }
