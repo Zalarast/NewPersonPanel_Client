f2141d3ac43096fde15c4bd7aadb1fac5d5411e2
Zalarast <shubin.bodya@gmail.com>
2022-08-12 16:08:45 +0300

    Событие при прогрузке страницы, при которой происходит получение ифнормации с сервера, изменил value на defaultValue для исправления ошибки в браузере

--- src/pages/Settings/Settings.tsx
+++ src/pages/Settings/Settings.tsx
@@ -1,27 +1,30 @@
 import React from "react"
 import "../../CSS/Settings.css"
 import { SettingsInfo } from "../../types"
+import { getInfo } from "./function"
 
 export default function Settings() {
-    const [settingsData, setSettingsData] = React.useState<SettingsInfo>({})
+    const [settingsData, setSettingsData] = React.useState<SettingsInfo>({ ADInfo: { ldap: null, login: null }, userInfo: { avatar: null, login: null } })
+    React.useEffect(() => { getInfo(setSettingsData).then(res => !res && alert("Произошёл сбой при получении данных")) }, [])
+
     return (
         <div className="Settings">
             <div className="settingsBlock">
                 <h1>Настройки пользователя</h1>
-                <img className="avatar" src={settingsData.userInfo?.avatar} alt="avatar" />
+                <img className="avatar" src={settingsData.userInfo?.avatar || ""} alt="avatar" />
                 <h2>Логин</h2>
-                <input value={settingsData.userInfo?.login}/>
+                <input defaultValue={settingsData.userInfo?.login || ""} />
                 <h2>Пароль</h2>
-                <input value={settingsData.userInfo?.pass} type="password" />
+                <input type="password" />
             </div>
             <div className="settingsBlock">
                 <h1>Настройка ActiveDirectory</h1>
                 <h2>ldap</h2>
-                <input value={settingsData.ADInfo?.ldap} />
+                <input defaultValue={settingsData.ADInfo?.ldap || ""} />
                 <h2>Логин</h2>
-                <input value={settingsData.ADInfo?.login}/>
+                <input defaultValue={settingsData.ADInfo?.login || ""} />
                 <h2>Пароль</h2>
-                <input value={settingsData.ADInfo?.pass} type="password" />
+                <input type="password" />
             </div>
         </div>
     )
